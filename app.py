807f860c8db0ce5b1c311ef969c05122bffab506
César Augusto Sibila <66441784+Czar210@users.noreply.github.com>
2024-06-26 21:32:15 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import pandas as pd
-import openpyxl
 
-# Carregar o arquivo Excel corrigido by puc zaras
+# Carregar o arquivo Excel corrigido
 file_path = 'LCK_Data.xlsx'
 df = pd.read_excel(file_path, sheet_name='Sheet1')
 
-# A função de encontrar nome
+# Função para encontrar nome
 def find_team_by_name(team_name):
     team_info = df[df['Name'].str.contains(team_name, case=False, na=False)]
     if team_info.empty:
@@ -14,7 +13,7 @@ def find_team_by_name(team_name):
     else:
         return team_info
 
-# A função que tu escreveu no teu codigo
+# Função para calcular winrate
 def calcular_winrate(df, time_a, time_b):
     resultado_a = df.loc[df['Name'] == time_a, 'Result'].values[0]
     resultado_b = df.loc[df['Name'] == time_b, 'Result'].values[0]
@@ -23,15 +22,16 @@ def calcular_winrate(df, time_a, time_b):
     produto_b = resultado_b * numero_necessario
     return numero_necessario, produto_a, produto_b
 
-#O titulo da pagina mude avontade
+# Título da página
 st.title('Calculadora de Winrate de Times')
 
-#A seleção de times é isso aqui
+# Seleção de times
 team_names = df['Name'].unique()
 time_a = st.selectbox('Selecione o primeiro time', team_names)
-time_b = st.selectbox('Selecione o segundo time', team_names)
+time_b_options = [team for team in team_names if team != time_a]
+time_b = st.selectbox('Selecione o segundo time', time_b_options)
 
-#Butão que faz calcular a winrate dos times que você selecionou
+# Botão para calcular a winrate dos times selecionados
 if st.button('Calcular Winrate'):
     numero, winrate_a, winrate_b = calcular_winrate(df, time_a, time_b)
     results_df = pd.DataFrame({
